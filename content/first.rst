9e064fd56da428d53f677253387bb26beea24284
a.kutsevol <a.kutsevol@lucky-labs.com>
2017-01-22 19:46:47 +0200

    First commit with blog

---
+++ content/first.rst
@@ -0,0 +1,25 @@
+.. |date| date:: %Y-%m-%d
+.. |time| date:: %H:%M
+
+:title: HELLO WORLD!!!
+:date: 2017-01-20 13:45
+:modified: |date| |time|
+:author: Artur K.
+:category: test
+:tags: python,blog
+:slug: first.rst
+
+What |time| is it?
+
+Моя первая статья используя `Pelican <http://docs.getpelican.com/>`_!
+
+.. code-block:: python
+    
+    def foo(): 
+	pass
+
+.. code-block:: python
+
+    def bar():
+	pass
+
---
+++ pelicanconf.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Artur Kutsevol'
+SITENAME = 'Artur Kutsevol Blog'
+SITEURL = 'https://kutsevol.github.io/'
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Kiev'
+
+DEFAULT_LANG = 'ru'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+GITHUB_URL = "https://github.com/kutsevol"
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('github', 'https://github.com/kutsevol'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+THEME =  "themes/pelican-elegant"
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
