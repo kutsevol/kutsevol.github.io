86b027e08bc0e4b1c5e2b9db7e3cc72c6f213ca9
a.kutsevol <a.kutsevol@lucky-labs.com>
2017-01-24 13:32:01 +0200

    Structure all parameters

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,8 +5,17 @@ from __future__ import unicode_literals
 AUTHOR = 'Artur Kutsevol'
 SITENAME = 'Artur Kutsevol'
 # SITEURL = 'https://kutsevol.github.io'
-SITEURL = ''
+SITEURL = 'http://localhost:8000'
 
+# Regional Settings
+TIMEZONE = 'Europe/Kiev'
+DEFAULT_LANG = 'ru'
+
+# Appearance
+THEME = "themes/elegant"
+DEFAULT_PAGINATION = False
+
+# Paths and urls
 PATH = 'content'
 
 ARTICLE_PATHS = ('articles',)
@@ -20,28 +29,21 @@ PAGE_SAVE_AS = "{slug}/index.html"
 STATIC_PATHS = ('images', 'extra/favicon.ico')
 EXTRA_PATH_METADATA = {'extra/favicon.ico': {'path': 'favicon.ico'}}
 
-TIMEZONE = 'Europe/Kiev'
-
-DEFAULT_LANG = 'ru'
+MENUITEMS = (('About', '/about-me/index.html'),)
+YEAR_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/index.html'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
+# AUTHOR_FEED_ATOM = None
+# AUTHOR_FEED_RSS = None
 
 # Social widget
 SOCIAL = (('Github', 'https://github.com/kutsevol'),
           ('Linkedin', 'https://www.linkedin.com/in/arthur-kutsevol-a20731a4'),
           )
 
-DEFAULT_PAGINATION = 10
-DISPLAY_RECENT_POSTS_ON_SIDEBAR = True
-
-MENUITEMS = (('About', '/about-me/index.html'),)
-YEAR_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/index.html'
-MONTH_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/index.html'
-THEME = "themes/elegant"
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
